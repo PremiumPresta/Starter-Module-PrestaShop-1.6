fd47340f57160136a2f134f4f5cea63565063392
Gabriel Arama <arama.gabriel@gmail.com>
2016-05-25 20:34:57 +0300

    sql unsigned id declaration
    id_quote was a signed int

--- starterpsmodule/sql/install.php
+++ starterpsmodule/sql/install.php
@@ -10,7 +10,7 @@
 $sql = array();
 
 $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'quotes` (
-        `id_quote` int(11) NOT NULL AUTO_INCREMENT,
+        `id_quote` int(10) unsigned NOT NULL AUTO_INCREMENT,
         PRIMARY KEY  (`id_quote`)
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
 
