6e485de61237f8899b675b81cd15fd7a140cbccb
Gabriel Arama <arama.gabriel@gmail.com>
2017-10-20 17:52:56 +0300

    install sql simple execute test

--- starterpsmodule/sql/install.php
+++ starterpsmodule/sql/install.php
@@ -15,7 +15,5 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'quotes` (
 ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
 
 foreach ($sql as $query) {
-    if (Db::getInstance()->execute($query) == false) {
-        return false;
-    }
+    if (!Db::getInstance()->execute($query)) return false;
 }
