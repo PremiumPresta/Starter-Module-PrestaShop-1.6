9d6878ee6bc0951a7ad9f8f94cf62f78a4aa9c7d
Gabriel Arama <arama.gabriel@gmail.com>
2017-10-20 20:07:40 +0300

    uninstall sql simple execute test

--- starterpsmodule/sql/uninstall.php
+++ starterpsmodule/sql/uninstall.php
@@ -11,7 +11,5 @@ $sql = array();
 $sql[] = 'DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'quotes`;';
 
 foreach ($sql as $query) {
-    if (Db::getInstance()->execute($query) == false) {
-        return false;
-    }
+    if (!Db::getInstance()->execute($query)) return false;
 }
